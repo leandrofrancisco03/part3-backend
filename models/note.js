d6c298f2884946fd6d876b56ce9d469b8494c3e7
Francisco <2020011024@unfv.edu.pe>
2025-06-27 22:15:38 -0500

    finalizado

--- models/note.js
+++ models/note.js
@@ -2,26 +2,23 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-
 const url = process.env.MONGODB_URI
 
-
 console.log('connecting to', url)
-
-mongoose.connect(url)
-
-  .then(result => {
+mongoose
+  .connect(url)
+  .then((result) => {
     console.log('connected to MongoDB')
   })
-  .catch(error => {
+  .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    minlength: 5,
+    required: true,
   },
   important: Boolean,
 })
@@ -31,8 +28,7 @@ noteSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
